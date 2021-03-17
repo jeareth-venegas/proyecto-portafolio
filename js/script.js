266245556435ee3aef5cab060bc9d7936f26d33c
Jeareth Venegas <venegasjea@gmail.com>
2021-03-17 14:44:19 -0600

    repositorios en el portfolio.html

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// url del api de github
+const linkGithub = 'https://api.github.com/users/jeareth-venegas/repos';
+
+fetch(linkGithub)
+  .then((response) => response.json())
+  .then((data) => {
+    const liInfo = document.querySelector('list__github');
+    for (let i = 0; i < data.length; i += 1) {
+    // li en html para el ul dentro del porfolio.html
+      const li = document.createElement('li');
+      liInfo.appendChild(li);
+      // creo un a para hacer un href con el link del repo
+      // una lista de todos los repositorio
+      const nameRepository = document.createElement('a');
+      nameRepository.setAttribute('href', `${data[i].html_url}`);
+      nameRepository.innerHTML = data[i].name;
+      li.appendChild(nameRepository);
+    }
+  });
